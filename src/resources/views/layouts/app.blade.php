af5a604821e2244f4ca96948b562ba7b68b27c86
omine emi <rep.pro.o.e@gmail.com>
2026-02-20 00:36:45 +0900

    wip: app.blade.phpレイアウト途中実装

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'COACHTECH')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css')}}">
+    @yield('css')
+</head>
+
+<body>
+    <div class="app">
+        <header class="header">
+            <div class="header__inner">
+                <div class="header__logo">
+                    <a href="/">
+                        <img src="{{ asset('images/logo/coachtech.png') }}" alt="COACHTECH">
+                    </a>
+                </div>
+
+        <nav class="header__nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="/attendance" class="nav-link">勤怠</a></li>
+                <li class="nav-item"><a href="/attendance/list" class="nav-link">勤怠一覧</a></li>
+                <li class="nav-item"><a href="/application" class="nav-link">申請</a></li>
+                @auth
+                <li class="nav-item">
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="nav-link-btn">ログアウト</button>
+                    </form>
+                </li>
+                @endauth
+            </ul>
+        </nav>
+    </div>
+</header>
