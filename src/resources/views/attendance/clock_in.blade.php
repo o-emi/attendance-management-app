5cffdef09d5512e3bce8edcd8108d315ff1bc550
omine emi <rep.pro.o.e@gmail.com>
2026-02-20 00:33:24 +0900

    feat.勤怠登録画面、CSSファイル作成（仮）

---
+++ src/resources/views/attendance/clock_in.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '勤怠登録画面')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/attendance/clock_in.css')}}">
+@endsection
+
+@section('content')
+<h1>勤怠登録画面</h1>
+@endsection
