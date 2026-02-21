a55fd5d871945d0d2e30266e721374774d497ab0
omine emi <rep.pro.o.e@gmail.com>
2026-02-21 16:12:24 +0900

    fix: Fortify登録処理の引数不足とリダイレクト先を修正

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Fortify\Http\Requests\LoginRequest;
-use Laravel\Fortify\Http\Requests\RegisterRequest;
 use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController as FortifyLogin;
 use Laravel\Fortify\Http\Controllers\RegisteredUserController as FortifyRegister;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class AuthController extends Controller
 {
     public function loginRedirect(LoginRequest $request)
     {
         app(FortifyLogin::class)->store($request);
-        return redirect('/attendance/clock_in');
+        return redirect()->route('attendance');
     }
 
-    public function registerRedirect(RegisterRequest $request)
+    public function registerRedirect(Request $request)
     {
-        app(FortifyRegister::class)->store($request);
-        return redirect('/attendance/clock_in');
+        app(FortifyRegister::class)->store(
+            $request,
+            app(CreatesNewUsers::class)
+        );
+        return redirect()->route('attendance');
     }
 }
