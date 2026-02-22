710b59d03c3d9d58e0bbf2565084e70970c00b09
omine emi <rep.pro.o.e@gmail.com>
2026-02-22 23:25:37 +0900

    fix: layoutにmainタグとcontent出力を追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -20,20 +20,27 @@
                     </a>
                 </div>
 
-        <nav class="header__nav">
-            <ul class="nav-list">
-                <li class="nav-item"><a href="/attendance" class="nav-link">勤怠</a></li>
-                <li class="nav-item"><a href="/attendance/list" class="nav-link">勤怠一覧</a></li>
-                <li class="nav-item"><a href="/application" class="nav-link">申請</a></li>
-                @auth
-                <li class="nav-item">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="nav-link-btn">ログアウト</button>
-                    </form>
-                </li>
-                @endauth
-            </ul>
-        </nav>
+                <nav class="header__nav">
+                    <ul class="nav-list">
+                        <li class="nav-item"><a href="/attendance" class="nav-link">勤怠</a></li>
+                        <li class="nav-item"><a href="/attendance/list" class="nav-link">勤怠一覧</a></li>
+                        <li class="nav-item"><a href="/application" class="nav-link">申請</a></li>
+                        @auth
+                        <li class="nav-item">
+                            <form   form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="nav-link-btn">ログアウト</button>
+                            </form>
+                        </li>
+                        @endauth
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <main class="main">
+            @yield('content')
+        </main>
+
     </div>
-</header>
+</body>
