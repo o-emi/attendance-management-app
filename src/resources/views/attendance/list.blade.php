e46dd30b2afa158fb6bca8d3785b1d10e51bb0a1
omine emi <rep.pro.o.e@gmail.com>
2026-02-22 23:24:27 +0900

    refactor: 勤怠一覧画面のファイル名を統一

---
+++ src/resources/views/attendance/list.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '勤怠一覧')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/attendance/list.css')}}">
+@endsection
+
+@section('content')
+<h1>勤怠一覧</h1>
+@endsection
