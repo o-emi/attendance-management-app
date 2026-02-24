a222ddff9537b7684fb27f53940ed1d856a2a5d1
omine emi <rep.pro.o.e@gmail.com>
2026-02-25 01:41:06 +0900

    勤怠一覧（管理者）のblade、cssファイルのみ作成

---
+++ src/resources/views/admin/attendance/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.login_register')
+
+@section('title', '勤怠一覧(管理者)')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/admin/attendance/index.css')}}">
+@endsection
+
+@section('content')
+<h1>勤怠一覧（管理者）</h1>
+@endsection
