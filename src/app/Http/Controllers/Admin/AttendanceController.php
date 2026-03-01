77b4f56c36b95feca49769804adf4aaecc9aa711
omine emi <rep.pro.o.e@gmail.com>
2026-03-02 01:46:48 +0900

    管理者勤怠一覧機能を実装（日付対応・詳細表示追加）

--- src/app/Http/Controllers/Admin/AttendanceController.php
+++ src/app/Http/Controllers/Admin/AttendanceController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Carbon\Carbon;
 use App\Models\Attendance;
 
 class AttendanceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $attendances = Attendance::with('user')
-            ->latest()
-            ->get();
+        $date = $request->input('date', Carbon::today()->toDateString());
 
-        return view('admin.attendance.list', compact('attendances'));
+        $users = User::with(['attendances' => function ($query) use ($date) {
+        $query->whereDate('work_date', $date);
+        }])->get();
+
+        return view('admin.attendance.list', compact('users', 'date'));
+    }
+
+    public function show(Attendance $attendance)
+    {
+        return view('admin.attendance.show', compact('attendance'));
     }
 }
