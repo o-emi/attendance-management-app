b13f6d0293d96249ad38ac0ec97dbab1a19cada5
omine emi <rep.pro.o.e@gmail.com>
2026-03-11 01:29:11 +0900

    feat(attendance): 勤怠一覧で出勤・退勤・休憩・勤務時間を表示

--- src/resources/views/attendance/list.blade.php
+++ src/resources/views/attendance/list.blade.php
@@ -3,19 +3,39 @@
 @section('title', '勤怠一覧')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/attendance/list.css')}}"><link rel="stylesheet" href="{{ asset('css/attendance/layouts/common.css')}}">
+<link rel="stylesheet" href="{{ asset('css/attendance/layouts/common.css') }}">
+<link rel="stylesheet" href="{{ asset('css/attendance/list.css') }}">
 @endsection
 
 @section('content')
-<div class="attendance"> <h2 class="attendance__title">勤怠一覧</h2> {{-- 月選択ナビゲーション --}}
-    <div class="attendance__nav month-selector"> <a href="#" class="month-selector__link month-selector__link--prev">← 前月</a> <div class="month-selector__current">
-            <span class="month-selector__icon">📅</span> 2023/06
+<div class="attendance">
+
+    <h2 class="attendance__title">勤怠一覧</h2>
+
+    {{-- 月選択ナビゲーション --}}
+    <div class="attendance__nav month-selector">
+
+        <a href="#" class="month-selector__link month-selector__link--prev">
+            ← 前月
+        </a>
+
+        <div class="month-selector__current">
+            <span class="month-selector__icon">📅</span>
+            2023/06
         </div>
-        <a href="#" class="month-selector__link month-selector__link--next">翌月 →</a> </div>
+
+        <a href="#" class="month-selector__link month-selector__link--next">
+            翌月 →
+        </a>
+
+    </div>
 
     {{-- 勤怠テーブル --}}
     <div class="attendance__table-wrapper">
-        <table class="attendance-table"> <thead class="attendance-table__head">
+
+        <table class="attendance-table">
+
+            <thead class="attendance-table__head">
                 <tr class="attendance-table__row">
                     <th class="attendance-table__header">日付</th>
                     <th class="attendance-table__header">出勤</th>
@@ -25,31 +45,70 @@
                     <th class="attendance-table__header">詳細</th>
                 </tr>
             </thead>
+
             <tbody class="attendance-table__body">
-                {{-- データがある行 --}}
-                <tr class="attendance-table__row">
-                    <td class="attendance-table__item">06/01(木)</td>
-                    <td class="attendance-table__item">09:00</td>
-                    <td class="attendance-table__item">18:00</td>
-                    <td class="attendance-table__item">1:00</td>
-                    <td class="attendance-table__item">8:00</td>
-                    <td class="attendance-table__item">
-                        <a href="#" class="attendance-table__link">詳細</a>
-                    </td>
-                </tr>
-                {{-- 休日などデータがない行 (Modifierでグレーアウト等の制御も可能) --}}
-                <tr class="attendance-table__row attendance-table__row--empty">
-                    <td class="attendance-table__item">06/04(日)</td>
-                    <td class="attendance-table__item"></td>
-                    <td class="attendance-table__item"></td>
-                    <td class="attendance-table__item"></td>
-                    <td class="attendance-table__item"></td>
-                    <td class="attendance-table__item">
-                        <a href="#" class="attendance-table__link">詳細</a>
-                    </td>
-                </tr>
+
+            @foreach($attendances as $attendance)
+
+            @php
+                $breakTotal = 0;
+
+                foreach ($attendance->breakTimes as $break) {
+                    if ($break->break_start && $break->break_end) {
+                        $breakTotal += \Carbon\Carbon::parse($break->break_start)
+                            ->diffInMinutes(\Carbon\Carbon::parse($break->break_end));
+                    }
+                }
+
+                $workTotal = 0;
+
+                if ($attendance->clock_in && $attendance->clock_out) {
+                    $workTotal = \Carbon\Carbon::parse($attendance->clock_in)
+                        ->diffInMinutes(\Carbon\Carbon::parse($attendance->clock_out)) - $breakTotal;
+                }
+
+                $breakHour = floor($breakTotal / 60);
+                $breakMinute = $breakTotal % 60;
+
+                $workHour = floor($workTotal / 60);
+                $workMinute = $workTotal % 60;
+            @endphp
+
+            <tr class="attendance-table__row">
+
+            <td class="attendance-table__item">
+            {{ \Carbon\Carbon::parse($attendance->work_date)->format('m/d(D)') }}
+            </td>
+
+            <td class="attendance-table__item">
+            {{ optional($attendance->clock_in)->format('H:i') }}
+            </td>
+
+            <td class="attendance-table__item">
+            {{ optional($attendance->clock_out)->format('H:i') }}
+            </td>
+
+            <td class="attendance-table__item">
+            {{ sprintf('%d:%02d', $breakHour, $breakMinute) }}
+            </td>
+
+            <td class="attendance-table__item">
+            {{ sprintf('%d:%02d', $workHour, $workMinute) }}
+            </td>
+
+            <td class="attendance-table__item">
+            <a href="#" class="attendance-table__link">詳細</a>
+            </td>
+
+            </tr>
+
+            @endforeach
+
             </tbody>
+
         </table>
+
     </div>
+
 </div>
 @endsection
