503a5a500a302cb7754df087c1014ade6b8b3652
omine emi <rep.pro.o.e@gmail.com>
2026-03-18 02:05:05 +0900

    refactor(view): 勤怠詳細ビューを@includeで分割

--- src/resources/views/attendance/show.blade.php
+++ src/resources/views/attendance/show.blade.php
@@ -44,85 +44,18 @@
                     </div>
                 </div>
 
-                {{-- 出勤・退勤 --}}
-                <div class="attendance-detail__group">
-                    <label class="attendance-detail__label">出勤・退勤</label>
-                    <div class="attendance-detail__content">
-                        <div class="attendance-detail__input-row">
-                            <input type="time" name="clock_in" class="attendance-detail__input"
-                                value="{{ old('clock_in', $attendance->clock_in?->format('H:i')) }}"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
-                            <span class="attendance-detail__separator">〜</span>
-                            <input type="time" name="clock_out" class="attendance-detail__input"
-                                value="{{ old('clock_out', $attendance->clock_out?->format('H:i')) }}"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
-                        </div>
-                        @error('clock_out')
-                            <p class="attendance-detail__error-text">{{ $message }}</p>
-                        @enderror
-                    </div>
-                </div>
+                @include('attendance.partials.clock-row')
 
-                {{-- 休憩時間（ループ処理） --}}
                 @foreach($attendance->breakTimes as $index => $break)
-                <div class="attendance-detail__group">
-                    <label class="attendance-detail__label">
-                        休憩{{ $index > 0 ? $index + 1 : '' }}
-                    </label>
-                    <div class="attendance-detail__content">
-                        <div class="attendance-detail__input-row">
-                            <input type="time" name="break_start[]" class="attendance-detail__input"
-                                value="{{ old('break_start.'.$index, $break->break_start ? \Carbon\Carbon::parse($break->break_start)->format('H:i') : '') }}"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
-                            <span class="attendance-detail__separator">〜</span>
-                            <input type="time" name="break_end[]" class="attendance-detail__input"
-                                value="{{ old('break_end.'.$index, $break->break_end ? \Carbon\Carbon::parse($break->break_end)->format('H:i') : '') }}"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
-                        </div>
-
-                        @error('break_end.'.$index)
-                            @if(old('break_start.'.$index) || old('break_end.'.$index))
-                                <p class="attendance-detail__error-text">{{ $message }}</p>
-                            @endif
-                        @enderror
-
-                    </div>
-                </div>
+                    @include('attendance.partials.break-row')
                 @endforeach
 
-                {{-- 追加の休憩入力 --}}
-                <div class="attendance-detail__group">
-                    <label class="attendance-detail__label">
-                        休憩{{ $attendance->breakTimes->count() + 1 }}
-                    </label>
-                    <div class="attendance-detail__content">
-                        <div class="attendance-detail__input-row">
+                @include('attendance.partials.break-row-add')
 
-                            <input type="time" name="break_start[]"     class="attendance-detail__input"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
+                @include('attendance.partials.remark-row')
 
-                            <span class="attendance-detail__separator">〜</span>
-
-                            <input type="time" name="break_end[]" class="attendance-detail__input"
-                                {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- 備考 --}}
-                <div class="attendance-detail__group">
-                    <label class="attendance-detail__label">備考</label>
-                    <div class="attendance-detail__content">
-                        <textarea name="remark" class="attendance-detail__textarea"
-                            {{ $attendance->status === '承認待ち' ? 'disabled' : '' }}>{{ old('remark', $attendance->remark) }}</textarea>
-                        @error('remark')
-                            <p class="attendance-detail__error-text">{{ $message }}</p>
-                        @enderror
-                    </div>
-                </div>
             </div>
 
-            {{-- 修正ボタン（承認待ちでない場合のみ表示） --}}
             @if($attendance->status !== '承認待ち')
             <div class="attendance-detail__actions">
                 <button type="submit" class="attendance-detail__submit-btn">修正</button>
@@ -135,4 +68,5 @@
         </form>
     </div>
 </div>
+
 @endsection
