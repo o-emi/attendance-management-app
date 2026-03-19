e69c4dcdfcc4fb3a21b308974d5708a6ae66c41a
omine emi <rep.pro.o.e@gmail.com>
2026-03-20 00:54:46 +0900

    refactor(admin-attendance-list): 時間計算をモデルアクセサに移行

--- src/resources/views/admin/attendance/list.blade.php
+++ src/resources/views/admin/attendance/list.blade.php
@@ -3,7 +3,7 @@
 @section('title', '勤怠一覧(管理者)')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/admin/attendance/list.css')}}">
+<link rel="stylesheet" href="{{ asset('css/admin/attendance/list.css') }}">
 @endsection
 
 @section('content')
@@ -33,6 +33,7 @@
 
         <div class="attendance__table-wrapper">
             <table class="attendance-table">
+
                 <thead class="attendance-table__head">
                     <tr class="attendance-table__row">
                         <th class="attendance-table__header">名前</th>
@@ -46,71 +47,52 @@
 
                 <tbody class="attendance-table__body">
 
-                @foreach($users as $user)
-                    @php
-                        $attendance = $user->attendances->first();
-                            $breakTotal = 0;
-                            $workTotal = 0;
-                            $actualWork = 0;
+                    @foreach($users as $user)
 
-                            if ($attendance) {
-
-                                foreach ($attendance->breakTimes as $break) {
-                                    if ($break->break_start && $break->break_end) {
-                                        $breakTotal += \Carbon\Carbon::parse($break->break_start)
-                                            ->diffInSeconds($break->break_end);
-                                    }
-                                }
-
-                                if ($attendance->clock_in && $attendance->clock_out) {
-                                    $workTotal = \Carbon\Carbon::parse($attendance->clock_in)
-                                        ->diffInSeconds($attendance->clock_out);
-
-                                    $actualWork = $workTotal - $breakTotal;
-                                }
-                            }
+                        @php
+                            $attendance = $user->attendances->first();
                         @endphp
 
-                    <tr class="attendance-table__row">
-                        <td class="attendance-table__item">
-                            {{ $user->name }}
-                        </td>
+                        <tr class="attendance-table__row">
 
-                        <td class="attendance-table__item">
-                            {{ $attendance?->clock_in
-                                ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i')
-                                : '' }}
-                        </td>
+                            <td class="attendance-table__item">
+                                {{ $user->name }}
+                            </td>
 
-                        <td class="attendance-table__item">
-                            {{ $attendance?->clock_out
-                                ? \Carbon\Carbon::parse($attendance->clock_out)->format('H:i')
-                                : '' }}
-                        </td>
+                            <td class="attendance-table__item">
+                                {{ $attendance?->clock_in?->format('H:i') }}
+                            </td>
 
-                        <td class="attendance-table__item">
-                            {{ $breakTotal ? gmdate('H:i', $breakTotal) : '' }}
-                        </td>
+                            <td class="attendance-table__item">
+                                {{ $attendance?->clock_out?->format('H:i') }}
+                            </td>
 
-                        <td class="attendance-table__item">
-                            {{ $actualWork ? gmdate('H:i', $actualWork) : '' }}
-                        </td>
+                            <td class="attendance-table__item">
+                                {{ $attendance?->break_total }}
+                            </td>
 
-                        <td class="attendance-table__item">
-                            @if($attendance)
-                                <a href="{{ route('admin.attendance.show', $attendance->id) }}"
-                                    class="attendance-table__link">
-                                    詳細
-                                </a>
-                            @endif
-                        </td>
-                    </tr>
+                            <td class="attendance-table__item">
+                                {{ $attendance?->work_total }}
+                            </td>
 
-                @endforeach
+                            <td class="attendance-table__item">
+                                @if($attendance)
+                                    <a href="{{ route('admin.attendance.show', $attendance->id) }}"
+                                        class="attendance-table__link">
+                                        詳細
+                                    </a>
+                                @endif
+                            </td>
+
+                        </tr>
+
+                    @endforeach
 
                 </tbody>
+
             </table>
         </div>
+
     </div>
 </div>
 @endsection
