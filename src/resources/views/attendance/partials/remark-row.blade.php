59621e41890feb27a4bd0df96a6385b795eb393a
omine emi <rep.pro.o.e@gmail.com>
2026-03-20 01:04:57 +0900

    refactor(attendance-show): 備考パーシャルの構造整理とエラー表示調整

--- src/resources/views/attendance/partials/remark-row.blade.php
+++ src/resources/views/attendance/partials/remark-row.blade.php
@@ -1,30 +1,17 @@
 <div class="attendance-detail__group">
-
     <label class="attendance-detail__label">備考</label>
 
     <div class="attendance-detail__content">
 
         @if($attendance->status === '承認待ち')
-
-            <span class="attendance-detail__text">
-                {{ $attendance->remark }}
-            </span>
-
+            <span class="attendance-detail__text">{{ $attendance->remark }}</span>
         @else
-
-            <textarea
-                name="remark"
-                class="attendance-detail__textarea"
-            >{{ old('remark', $attendance->remark) }}</textarea>
-
+            <textarea name="remark" class="attendance-detail__textarea">{{ old('remark', $attendance->remark) }}</textarea>
         @endif
-
-        @error('remark')
-            <p class="attendance-detail__error-text">
-                {{ $message }}
-            </p>
-        @enderror
 
     </div>
 
+    @error('remark')
+        <p class="attendance-detail__error-text">{{ $message }}</p>
+    @enderror
 </div>
