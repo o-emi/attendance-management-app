9d823d5b447a15e61413782d17e26c96cedfb92e
omine emi <rep.pro.o.e@gmail.com>
2026-03-21 00:51:28 +0900

    feat(view): 承認待ち時の休憩追加入力を非表示化

--- src/resources/views/attendance/partials/break-row-add.blade.php
+++ src/resources/views/attendance/partials/break-row-add.blade.php
@@ -1,19 +1,13 @@
+@if($attendance->status !== '承認待ち')
 <div class="attendance-detail__group">
     <label class="attendance-detail__label">
         休憩{{ $attendance->breakTimes->count() + 1 }}
     </label>
 
     <div class="attendance-detail__content attendance-detail__input-row">
-
-        @if($attendance->status === '承認待ち')
-            <span class="attendance-detail__text"></span>
-            <span class="attendance-detail__separator">〜</span>
-            <span class="attendance-detail__text"></span>
-        @else
-            <input type="time" name="break_start[]" class="attendance-detail__input">
-            <span class="attendance-detail__separator">〜</span>
-            <input type="time" name="break_end[]" class="attendance-detail__input">
-        @endif
-
+        <input type="time" name="break_start[]" class="attendance-detail__input">
+        <span class="attendance-detail__separator">〜</span>
+        <input type="time" name="break_end[]" class="attendance-detail__input">
     </div>
 </div>
+@endif
