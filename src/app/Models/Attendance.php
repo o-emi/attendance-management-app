a2a765252add142829eb23f766c9c5c85eb23b52
omine emi <rep.pro.o.e@gmail.com>
2026-03-25 00:54:15 +0900

    feat(attendance-request): 修正申請リレーションを複数対応に変更

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\BreakTime;
+use App\Models\CorrectionRequest;
 use Carbon\Carbon;
 
 class Attendance extends Model
@@ -65,8 +66,8 @@ class Attendance extends Model
         return gmdate('H:i', $this->work_total_seconds);
     }
 
-    public function correctionRequest()
+    public function correctionRequests()
     {
-        return $this->hasOne(CorrectionRequest::class);
+        return $this->hasMany(CorrectionRequest::class);
     }
 }
