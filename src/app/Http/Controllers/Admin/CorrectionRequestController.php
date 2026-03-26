1bd12a67881f30b695d8a3b21aba7b9a291f8955
omine emi <rep.pro.o.e@gmail.com>
2026-03-27 00:58:08 +0900

    feat: 管理者用修正申請コントローラと承認処理を実装

---
+++ src/app/Http/Controllers/Admin/CorrectionRequestController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\CorrectionRequest;
+use Illuminate\Support\Facades\DB;
+
+class CorrectionRequestController extends Controller
+{
+    public function index()
+    {
+        $requests = CorrectionRequest::with('user')
+            ->latest()
+            ->get();
+
+        return view('admin.correction_requests.index', compact('requests'));
+    }
+
+    public function show($id)
+    {
+        $correctionRequest = CorrectionRequest::with('attendance.breakTimes', 'user')
+            ->findOrFail($id);
+
+        return view('admin.correction_requests.show', compact('correctionRequest'));
+    }
+
+    public function approve($id)
+    {
+        $correctionRequest = CorrectionRequest::with('attendance')
+            ->findOrFail($id);
+
+        DB::transaction(function () use ($correctionRequest) {
+
+            $attendance = $correctionRequest->attendance;
+
+            $correctionRequest->status = 'approved';
+            $correctionRequest->save();
+
+            $attendance->clock_in = $correctionRequest->clock_in;
+            $attendance->clock_out = $correctionRequest->clock_out;
+            $attendance->save();
+
+            $attendance->breakTimes()->delete();
+
+            foreach ($correctionRequest->break_times as $break) {
+                $attendance->breakTimes()->create([
+                    'start' => $break['start'],
+                    'end'   => $break['end'],
+                ]);
+            }
+        });
+
+        return redirect()->route('admin.request.list')
+            ->with('success', '承認しました');
+    }
+}
