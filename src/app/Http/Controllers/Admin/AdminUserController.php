89925c9fbc00a7b35ca6341a1fd4bf4efec2814f
omine emi <rep.pro.o.e@gmail.com>
2026-04-01 00:27:55 +0900

    feat: スタッフ別勤怠一覧表示処理を追加

--- src/app/Http/Controllers/Admin/AdminUserController.php
+++ src/app/Http/Controllers/Admin/AdminUserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Attendance;
+use Carbon\Carbon;
 
 class AdminUserController extends Controller
 {
@@ -13,5 +15,22 @@ class AdminUserController extends Controller
         $users = User::where('role', 'user')->get();
 
         return view('admin.staff.index', compact('users'));
+    }
+
+    public function showAttendance($id)
+    {
+        $user = User::findOrFail($id);
+
+        $attendances = Attendance::where('user_id', $id)
+            ->orderBy('work_date', 'desc')
+            ->get();
+
+        $currentMonth = Carbon::now()->format('Y年m月');
+
+        return view('admin.staff.attendance', compact(
+            'user',
+            'attendances',
+            'currentMonth'
+        ));
     }
 }
