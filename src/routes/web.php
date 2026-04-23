7bb044715cd7a902e8f2edeb6a8ed447941118be
omine emi <rep.pro.o.e@gmail.com>
2026-04-24 00:54:12 +0900

    feat: 管理者の月次勤怠一覧にCSV出力機能を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -111,4 +111,7 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function (){
 
     Route::get('/attendance/staff/{id}', [AdminUserController::class, 'showAttendance'])
         ->name('admin.staff.attendance');
+
+    Route::get('/attendance/staff/{id}/csv',[AdminUserController::class, 'exportCsv'])
+        ->name('admin.staff.attendance.csv');
 });
