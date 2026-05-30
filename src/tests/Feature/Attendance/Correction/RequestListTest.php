e3cc6c2d940966a19f535075d6c77d6efa202be2
omine emi <rep.pro.o.e@gmail.com>
2026-05-30 12:29:11 +0900

    test: 詳細画面遷移テストを追加

--- src/tests/Feature/Attendance/Correction/RequestListTest.php
+++ src/tests/Feature/Attendance/Correction/RequestListTest.php
@@ -74,4 +74,34 @@ class RequestListTest extends TestCase
         $response->assertSee($attendance->work_date->format('Y/m/d'));
         $response->assertSee($correctionRequest->note);
     }
+    // 勤怠詳細画面に遷移する
+    public function test_detail_button_redirects_to_attendance_detail()
+    {
+        $user = User::factory()->create();
+
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2026-05-30',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+        ]);
+
+        $correctionRequest = CorrectionRequest::create([
+        'user_id' => $user->id,
+        'attendance_id' => $attendance->id,
+        'start_time' => '10:00',
+        'end_time' => '19:00',
+        'note' => '電車遅延',
+        'status' => 'pending',
+    ]);
+        $response = $this->actingAs($user)
+            ->get(route('stamp_correction_request.list'));
+
+        $response->assertStatus(200);
+        $response->assertSee('詳細');
+        $response->assertSee('/attendance/detail/' . $attendance->id, false);
+        $detailResponse = $this->actingAs($user)
+            ->get(route('attendance.show', $attendance->id));
+        $detailResponse->assertStatus(200);
+    }
 }
