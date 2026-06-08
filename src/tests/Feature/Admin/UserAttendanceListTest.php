3b35e7da2555c496882256951f7efc468fdf5e75
omine emi <rep.pro.o.e@gmail.com>
2026-06-08 14:49:16 +0900

    管理者ユーザー勤怠一覧月表示テスト追加

--- src/tests/Feature/Admin/UserAttendanceListTest.php
+++ src/tests/Feature/Admin/UserAttendanceListTest.php
@@ -50,4 +50,67 @@ class UserAttendanceListTest extends TestCase
         $response->assertSee('08:00');
     }
 
+    public function test_previous_month_attendance_is_displayed()
+    {
+        Carbon::setTestNow('2026-06-01 09:00:00');
+
+        $admin = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        $user = User::factory()->create([
+            'role' => 'user',
+        ]);
+
+        Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2026-05-31',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+        ]);;
+
+        $response = $this->actingAs($admin)
+            ->get(route('admin.staff.attendance', [
+                'id' => $user->id,
+                'month' => '2026-05',
+        ]));
+
+        $response->assertStatus(200);
+        $response->assertSee('2026/05');
+
+        Carbon::setTestNow();
+    }
+
+    public function test_next_month_attendance_is_displayed()
+    {
+        Carbon::setTestNow('2026-06-01 09:00:00');
+
+        $admin = User::factory()->create([
+            'role' => 'admin'
+        ]);
+
+        $user = User::factory()->create([
+            'role' => 'user',
+        ]);
+
+        Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => '2026-07-01',
+            'clock_in' => '09:00',
+            'clock_out' => '18:00',
+        ]);
+
+        $response = $this->actingAs($admin)
+            ->get(route('admin.staff.attendance', [
+                'id' => $user->id,
+                'month' => '2026-07',
+        ]));
+
+        $response->assertStatus(200);
+        $response->assertSee('2026/07');
+
+        Carbon::setTestNow();
+    }
+
+
 }
