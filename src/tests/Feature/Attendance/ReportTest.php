0dff9c28b994edd292bcf33f1ac4e40538c3dcf0
omine emi <rep.pro.o.e@gmail.com>
2026-07-15 00:30:11 +0900

    test: 今月の異常検知一覧表示テストを追加

--- src/tests/Feature/Attendance/ReportTest.php
+++ src/tests/Feature/Attendance/ReportTest.php
@@ -79,8 +79,46 @@ class ReportTest extends TestCase
         $response->assertSee('2025/11');
 
         Carbon::setTestNow();
+    }
 
+    public function test_this_month_anomaly_list_is_displayed()
+    {
+        $user = User::factory()->create();
 
+        Carbon::setTestNow(Carbon::create(2026, 4, 16));
+
+        Attendance::factory()->create([
+            'user_id' => $user->id,
+            'work_date' => '2026-04-15',
+            'clock_in' => '2026-04-16 10:00:00',
+            'clock_out' => '2026-04-16 17:00:00',
+        ]);
+
+        $attendance = Attendance::factory()->create([
+            'user_id' => $user->id,
+            'work_date' => '2026-04-16',
+            'clock_in' => '2026-04-16 8:00:00',
+            'clock_out' => '2026-04-16 20:00:00',
+        ]);
+
+        BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_start' => '2026-04-16 12:00:00',
+            'break_end' => '2026-04-16 13:00:00',
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('attendance.report'));
+
+        $response->assertStatus(200);
+
+        $response->assertSee('遅刻回数');
+        $response->assertSee('早退回数');
+        $response->assertSee('長時間労働日数');
+
+        $response->assertSee('1回');
+        $response->assertSee('1日');
 
     }
 }
