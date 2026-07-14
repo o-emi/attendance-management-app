d2afc3c4d3b677b83de7692e6ff207931039767c
omine emi <rep.pro.o.e@gmail.com>
2026-07-14 13:19:02 +0900

    test: 過去6か月の月次推移表示テストを追加

--- src/tests/Feature/Attendance/ReportTest.php
+++ src/tests/Feature/Attendance/ReportTest.php
@@ -3,11 +3,11 @@
 namespace Tests\Feature\Attendance;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Attendance;
 use App\Models\BreakTime;
+use Carbon\Carbon;
 
 class ReportTest extends TestCase
 {
@@ -47,6 +47,40 @@ class ReportTest extends TestCase
         $response->assertStatus(200);
 
         $response->assertSee('8h 0m');
+    }
+
+    public function test_past_6_monthly_reports_are_displayed()
+    {
+        $user = User::factory()->create();
+
+        Carbon::setTestNow(Carbon::create(2026, 4, 16));
+
+        for ($i = 5; $i >= 0; $i--) {
+
+            Attendance::factory()->create([
+                'user_id' => $user->id,
+                'work_date' => Carbon::now()->subMonths($i),
+                'clock_in' => Carbon::now()->subMonths($i)->setTime(10, 0, 0),
+                'clock_out' => Carbon::now()->subMonths($i)->setTime(19, 0, 0),
+            ]);
+        }
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('attendance.report'));
+
+        $response->assertStatus(200);
+
+        $response->assertSee('2026/04');
+        $response->assertSee('2026/03');
+        $response->assertSee('2026/02');
+        $response->assertSee('2026/01');
+        $response->assertSee('2025/12');
+        $response->assertSee('2025/11');
+
+        Carbon::setTestNow();
+
+
 
     }
 }
